1584638cb34723d50c9296a70b75bdc8ce70366e
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-06-29 02:32:25 +0200

    Actuallu updated JS

--- public/javascript.js
+++ public/javascript.js
@@ -1,44 +1,73 @@
-const ms = new MediaSource();
-const video = dom.video[0];
-const mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
+const socket = io(undefined, { path: '/io' });
 
-console.log(video);
+const joystick = document.querySelector('.joystick');
+const position = document.querySelector('.position');
 
-// Make sure mp4 mime is supported
-if (!MediaSource.isTypeSupported(mimeCodec)) {
-  alert('Your browser does not support MP4');
-}
-
-const socket = io();
-
-video.addEventListener('canplaythrough', e => console.log('PLAYIN'));
-
-video.src = window.URL.createObjectURL(ms);
-ms.addEventListener('sourceopen', sourceOpen);
-
-function sourceOpen(e) {
-  let mediaSource = this;
-  let sourceBuffer = ms.addSourceBuffer(mimeCodec);
-  let queue = [];
-  let i = 0;
-  socket.on('stream', data => {
-    queue.push(data);
-    if (i === 0) {
-      frame();
-    }
-    i++;
-    console.log(data);
-    // sourceBuffer.appendBuffer(data);
-  });
-
-  const frame = function() {
-    console.log('Called!');
-    if (queue.length) {
-      sourceBuffer.appendBuffer(queue.shift());
-    }
-  };
-
-  sourceBuffer.addEventListener('updateend', frame, false);
+let last = false;
+const event = name => {
+  if (last && last === name) return;
+  last = name;
+  position.className = `position ${name}`;
+  return socket.emit(name);
 };
 
-ms.addEventListener('sourceclose', function(e) {console.log("SOURCE CLOSED");}, false);
+Mousetrap.bind('left', e => event('left'));
+Mousetrap.bind('right', e => event('right'));
+Mousetrap.bind('up', e => event('forward'));
+Mousetrap.bind('down', e => event('backward'));
+Mousetrap.bind('space', e => event('stop'));
+Mousetrap.bind('escape', e => event('stop'));
+
+socket.emit('init');
+
+const controller = (sel, cb) => {
+  let start = false;
+
+  const begin = e => {
+    joystick.classList.remove('stop');
+    const client = e.clientX ? e : e.touches[0];
+    start = { x: client.clientX, y: client.clientY }
+  };
+  joystick.addEventListener('mousedown', begin);
+  joystick.addEventListener('touchstart', begin);
+
+  const end = e => {
+    joystick.classList.add('stop');
+    joystick.setAttribute('style', '');
+    start = false;
+    cb({ x: 0, y: 0 });
+  };
+  document.addEventListener('mouseup', end);
+  document.addEventListener('touchend', end);
+
+  const control = e => {
+    // Mousemove WHILE pressing it (not any other time)
+    if (!start) return;
+
+    const client = e.clientX ? e : e.touches[0];
+    const diff = {
+      x: client.clientX - start.x,
+      y: start.y - client.clientY
+    };
+    const divisor = 2;
+    //joystick.style.marginLeft = diff.x;
+    joystick.setAttribute('style', 'margin-left: ' + (diff.x / divisor) + 'px; margin-top: ' + (-diff.y / divisor) + 'px;');
+    cb(diff);
+  };
+  document.addEventListener('mousemove', control);
+  document.addEventListener('touchmove', control);
+};
+
+controller('.joystick', diff => {
+
+  // If similar to 0, it means stop
+  if (Math.abs(diff.x) < 20 && Math.abs(diff.y) < 20) {
+    return event('stop');
+  }
+
+  if (Math.abs(diff.x) > Math.abs(diff.y)) {
+    return event(diff.x > 0 ? 'right' : 'left');
+  }
+
+  return event(diff.y > 0 ? 'forward' : 'backward');
+});
