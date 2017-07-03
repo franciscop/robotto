8c397b9430a3c27ca5b1a8b363833a86667aaf03
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-07-04 01:59:25 +0200

    Updated

--- motor.js
+++ motor.js
@@ -4,13 +4,12 @@ module.exports = (a, b, c) => ({
   forward: async () => {
     await gpio(a).on();
     await gpio(b).off();
-    if (c) {
-      await gpio(c).to(0.2);
-    }
+    await gpio(c).to(0.2);
   },
   backward: async () => {
     await gpio(a).off();
     await gpio(b).on();
+    await gpio(c).to(0.2);
   },
   stop: async () => {
     await gpio(a).off();
