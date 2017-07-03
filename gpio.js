a3a13ff50f536dc7dd946784b06cde668acd25e3
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-07-04 01:45:32 +0200

    Updated

--- gpio.js
+++ gpio.js
@@ -22,11 +22,15 @@ const handle = pin => ({
     value = 350;
     if (!modes[pin] || modes[pin] !== 'pwm') {
       console.log(`gpio mode ${pin} pwm`);
-      await exec(`gpio mode ${pin} pwm`);
+      console.log(`gpio mode 1 pwm`);
+      // await exec(`gpio mode ${pin} pwm`);
+      await exec(`gpio mode 1 pwm`);
       modes[pin] = 'pwm';
     }
     console.log(`gpio pwm ${pin} ${value}`);
-    await exec(`gpio pwm ${pin} ${value}`);
+    console.log(`gpio pwm 1 350`);
+    // await exec(`gpio pwm ${pin} ${value}`);
+    await exec(`gpio pwm 1 350`);
   }
 });
 
